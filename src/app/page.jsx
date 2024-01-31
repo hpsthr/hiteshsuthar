2fc46ce25ba8e8bb89d14b788f25ae2c3baa9b1d
hpsthr <hpsthr@gmail.com>
2024-01-31 13:21:07 +0530

    img

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,18 +14,20 @@ export default function Home() {
           <div className={classes["text-wrapper"]}>Work</div>
           <div className={classes.group}>
             <div className={classes["div-wrapper"]}>
-            
+              <Image width={1920} height={1080} className={classes["img1"]} alt="flex" src="/img/flex1.png" /> 
               <a href="./flexpay" className={classes.div}>Flex pay</a>
               
             </div>
           </div>
           <div className={classes["overlap-group-wrapper"]}>
-            <div className={classes["div-wrapper"]}>
+            <div className={classes["div-wrapper2"]}> 
+            <Image width={1920} height={1080} className={classes["img2"]} alt="Girl workout" src="/img/result1.png" />
               <a href="./resultonly" className={classes.div}>Results only fitness</a>
             </div>
           </div>
           <div className={classes.group2}>
-            <div className={classes["div-wrapper"]}>
+            <div className={classes["div-wrapper3"]}>
+            <Image width={1920} height={1080} className={classes["img3"]} alt="medical" src="/img/med.png" />
               <div className={classes.div}>Medical questionnaire</div>
             </div>
           </div>
@@ -46,8 +48,14 @@ export default function Home() {
       </p>
     </div>
         </div>
-      </div>
+        </div>
+        <div className={classes["text-wrapper-6"]}>Contact</div>
+        <div className={classes["text-wrapper-7"]}>Mobile : +919426364603</div>
+        <div className={classes["text-wrapper-8"]}>Email : hpsthr@gmail.com</div>
     </div>
+    
+        
+        
   </div>
   );
 }
